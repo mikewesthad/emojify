e6078480c5d23af70a75ad495d3a92e1602fbb7f
Michael Hadley <michaelwesthadley@gmail.com>
2016-04-12 10:32:07 -0500

    Updating the emoji-canvas comments

--- src/js/emoji-canvas.js
+++ src/js/emoji-canvas.js
@@ -1,6 +1,7 @@
 /**
  * A module that takes a clm tracker object and uses it to draw an emoji face
- * using p5.
+ * using p5.  The canvas has a debug mode that can be toggled on/off by pressing
+ * the "f" key.
  * @module emoji-canvas
  */
 
@@ -16,7 +17,11 @@ EmojiCanvas.prototype.start = function(width, height, parentElement) {
     //  https://github.com/processing/p5.js/wiki/Instantiation-Cases
     // This keeps everything from ending up in the global scope.
     var sketch = function (p) {
+        // Store p5 instance, so that not all methods have to be based it 
+        // directly
         this.p = p;
+        // Bind a context and some parameters to the p5 functions, so that when
+        // they are called by the p5 event loop, they have the info we want.
         p.setup = this._setup.bind(this, p, width, height);
         p.draw = this._draw.bind(this, p);
         p.keyPressed = this._keyPressed.bind(this, p);
@@ -30,15 +35,19 @@ EmojiCanvas.prototype.clear = function() {
 
 EmojiCanvas.prototype._setup = function(p, width, height) {
     var canvas = p.createCanvas(width, height);
-    p.textSize(32);
+    p.textSize(32); // Set the "default" text size for the canvas once
 };
 
 EmojiCanvas.prototype._draw = function(p) {
     p.clear();
+    // Get the array of face points from tracker, returns false if no face is 
+    // detected
     var facePoints = this.ctracker.getCurrentPosition();
     if (facePoints) {
         this._drawEmoji(p, facePoints);
         if (this.debugModeEnabled) {
+            // The score is a value between 0 and 1 that describes how well the
+            // "detected" face matches the tracker's face model 
             var score = this.ctracker.getScore();
             this._drawDebug(p, facePoints, score);
         }
@@ -66,6 +75,9 @@ EmojiCanvas.prototype._drawDebug = function(p, facePoints, score) {
 };
 
 EmojiCanvas.prototype._drawEmoji = function (p, facePoints) {
+    // clmtrackr has a reference diagram that shows what the array of facePoints
+    // correspond to in terms of the face model:
+    //  http://auduno.github.io/clmtrackr/docs/reference.html
     // Calculate the face diameter.  This is used to size the other face 
     // components.
     var nose = facePoints[41];
@@ -117,12 +129,14 @@ EmojiCanvas.prototype._drawMouth = function(p, facePoints, faceDiameter) {
     var irisDiameter = eyeDiameter * 0.5;    
     // Draw the mouth
     if (!isOpen) {
+        // Draw a straight line mouth
         p.stroke("#664e27");
         p.noFill();
         p.strokeWeight(10);  
         p.line(mouthLeft[0], mouthLeft[1], mouthRight[0], mouthRight[1]);
     }
     else {        
+        // Draw a half-circle mouth
         p.fill("#664e27");
         p.noStroke();
         p.push();
@@ -132,6 +146,8 @@ EmojiCanvas.prototype._drawMouth = function(p, facePoints, faceDiameter) {
         p.pop();
     }
 };
+
+// Helper functions that are private to this module
 
 function facePosDist(pos1, pos2) {
     var dx = pos2[0] - pos1[0];
